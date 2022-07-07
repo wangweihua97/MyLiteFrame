674758ab367cc4dd7e6de3089d5aae134fd1fa75
王伟华 <348031705@qq.com>
2022-07-07 21:33:29 +0800

    删除多余代码

--- Script/Events/EventInfo.cs
+++ Script/Events/EventInfo.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using BehaviorDesigner.Runtime.Tasks.Unity.UnityParticleSystem;
-using UnityEngine.Events;
+﻿using UnityEngine.Events;
 
 namespace Events
 {
